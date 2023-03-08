96a23374d5c9ff34938d2e68599fb0983a337b99
Dyuha <iamdyuha@tut.by>
2023-03-08 19:24:53 +0300

    головные мюсли

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import { Form } from './components/Form';
+import "./App.css";
+import { Form } from "./components/Form";
+import { ShowHistory } from "./components/ShowHistory";
 
 function App() {
   return (
-    <Form/>
-  )
+    <>
+      <Form />
+      <ShowHistory />
+    </>
+  );
 }
 
 export default App;
